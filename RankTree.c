e624beeb8501ecf7c04d51f643411466a01a1003
cersar <1319839558@qq.com>
2018-10-29 15:15:42 +0800

    Update RankTree.c

--- RankTree.c
+++ RankTree.c
@@ -1,13 +1,9 @@
-// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
 #include "pch.h"
 #include <iostream>
 #include <crtdbg.h>
 
 using namespace std;
-#define LEFT 0
-#define RIGHT 1
+
 #define BLACK 0
 #define RED 1
 
@@ -331,6 +327,25 @@ void RBDelete(RBTree *t, int key) {
 	}
 }
 
+TreeNode *find(RBTree t, int rank) {
+	TreeNode *p = t;
+	int r = size(p->left) + 1;
+	while (p != NULL) {
+		r = size(p->left) + 1;
+		if (rank < r) {
+			p = p->left;
+		}
+		else if (rank > r) {
+			rank -= r;
+			p = p->right;
+		}
+		else {
+			break;
+		}
+	}
+	return p;
+}
+
 int getRank(RBTree t, int key) {
 	TreeNode *p = t;
 	int r = 0;
@@ -348,131 +363,4 @@ int getRank(RBTree t, int key) {
 		}
 	}
 	return r;
-}
-
-struct LineNode {
-	double angle;
-	int ind;
-	double position;
-};
-
-void swap(int &a, int &b) {
-	int tmp = a;
-	a = b;
-	b = tmp;
-}
-
-int partation(double **a, int start, int end) {
-	int p = start - 1;
-	int q = end + 1;
-	double key = a[start][0];
-	while (true) {
-		do {
-			p++;
-		} while (a[p][0] < key);
-		do {
-			q--;
-		} while (a[q][0] > key);
-		if (p < q) {
-			swap(a[p], a[q]);
-		}
-		else {
-			return q;
-		}
-	}
-}
-
-void quickSort(double **a, int start, int end) {
-	int cnt = 0;
-	if (start<end) {
-		int p = partation(a, start, end);
-		quickSort(a, start, p);
-		quickSort(a, p+1, end);
-	}
-}
-
-int partation(LineNode *a, int start, int end) {
-	int p = start - 1;
-	int q = end + 1;
-	double key = a[start].angle;
-	while (true) {
-		do {
-			p++;
-		} while (a[p].angle < key);
-		do {
-			q--;
-		} while (a[q].angle > key);
-		if (p < q) {
-			swap(a[p], a[q]);
-		}
-		else {
-			return q;
-		}
-	}
-}
-
-void quickSort(LineNode *a, int start, int end) {
-	int cnt = 0;
-	if (start < end) {
-		int p = partation(a, start, end);
-		quickSort(a, start, p);
-		quickSort(a, p + 1, end);
-	}
-}
-
-void printLine(double a[2]) {
-	cout << "(" << a[0] << "," << a[1] << ")";
-}
-
-
-int main()
-{
-	double data[][2] = { {0.3,0.4},{0.1,0.5},{0.2,0.7},{0.6,0.9} };
-	int lineNum = sizeof(data) / sizeof(data[0]);
-	double **x = (double **)malloc(sizeof(double *) * lineNum);
-	for (int i = 0; i < lineNum; ++i) {
-		x[i] = (double *)malloc(sizeof(double) * 2);
-		for (int j = 0; j < 2; ++j) {
-			x[i][j]=data[i][j];
-		}
-	}
-
-	quickSort(x, 0, lineNum -1);
-	for (int i = 0; i < lineNum; ++i) {
-		for (int j = 0; j < 2; ++j) {
-			cout << x[i][j] << "\t" ;
-		}
-		cout << endl;
-	}
-	LineNode *nodes = (LineNode *)malloc(sizeof(LineNode)*lineNum * 2);
-	for (int i = 0; i < lineNum; ++i) {
-		nodes[2 * i].angle = x[i][0];
-		nodes[2 * i + 1].angle = x[i][1];
-		nodes[2 * i].position = LEFT;
-		nodes[2 * i + 1].position = RIGHT;
-		nodes[2 * i].ind = i;
-		nodes[2 * i + 1].ind = i;
-	}
-	quickSort(nodes, 0, lineNum * 2 - 1);
-	for (int i = 0; i < lineNum * 2; ++i) {
-		cout << nodes[i].angle << "\t" << nodes[i].ind << "\t" << nodes[i].position<<endl;
-	}
-	RBTree t=NULL;
-	int cnt = 0;
-	for (int i = 0; i < lineNum * 2; ++i) {
-		if (nodes[i].position == RIGHT) {
-			cnt += (t->size-getRank(t, nodes[i].ind));
-			RBDelete(&t, nodes[i].ind);
-			printInOrder(t);
-		}
-		else {
-			RBInsert(&t, nodes[i].ind, nodes[i].angle);
-		}
-	}
-	printInOrder(t);
-	cout << "相交弦数：" << cnt << endl;
-	FreeMemory(t);
-	free(x);
-	free(nodes);
-	_CrtCheckMemory();
 }
